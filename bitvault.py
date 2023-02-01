6089849ba951963ade5bebf9c9bf0a155ed7c135
arktikavenger <stevenwarren501@gmail.com>
2023-01-31 20:47:10 -0600

    Imported pynacl

--- bitvault.py
+++ bitvault.py
@@ -1,5 +1,8 @@
 # TODO: Password generator
-import json
+import nacl.secret
+import nacl.utils
+import nacl.pwhash
+import base64
 
 print("Welcome to BitVault, the simple and secure password manager for everyone!")
 
@@ -11,9 +14,9 @@ if signup_prompt.lower() == "yes":
     new_un = input("Please enter a username: ")
     new_pw = input("Please enter a password: ")
     user_list.append(new_un + " " + new_pw)
-    with open(r'C:\Users\steve\Coding\PasswordManager\UserList.txt', 'a') as ul:
+    with open(r'UserList.txt', 'a') as ul:  
         ul.write(str(user_list[0]))
-        ul.close()
+        pass
 
 if signup_prompt.lower() == "no":
     k = 1
@@ -22,19 +25,17 @@ if signup_prompt.lower() == "no":
             user = input("Please enter your username: ")
             pass_ = input("Please enter your password: ")
             user_list2.append([user, pass_])
-            p = pass_
             a = open(r'.\UserList.txt', 'r')
             ultext = a.read()
             
             if user and pass_ in ultext:
                 print("Logging in...")
+                p = pass_
                 break
                 k += 1
             if user or pass_ not in ultext:
                 print("Username or Password incorrect. Please try again.")
-                    
 
-#readlines() turns txt file into list, len determines number of lines
 
 use = input("Would you like to add a password or retrieve a password? " )
 
@@ -70,4 +71,30 @@ if use.lower() == "retrieve a password":
         print("Access Granted!")
     retrieve = input("Would you like to access all passwords or a specific password?")
 
+
+
+
+
+password = b"I like Python"
+secret_msg = b"Actually, I prefer Javascript..."
+
+# Generate the key:
+kdf = nacl.pwhash.argon2i.kdf # our key derivation function
+salt_size = nacl.pwhash.argon2i.SALTBYTES # The salt musts have a size of 16 bytes
+salt = nacl.utils.random(salt_size) # can be sth like: b'3\xba\x8f\r]\x1c\xcbOsU\x12\xb6\x9c(\xcb\x94'
+print(salt) # To decrypt the data later, you have to save this salt somewhere.
+key = kdf(nacl.secret.SecretBox.KEY_SIZE, password, salt)
+
+# Encrypt the data:
+box = nacl.secret.SecretBox(key)
+encrypted = box.encrypt(secret_msg)
+
+# Store the data with binary mode:
+with open('file.bin', 'wb') as f:
+  f.write(encrypted)
+
+# Store the data with text mode:
+with open('file.txt', 'w') as f:
+  content = base64.b64encode(encrypted).decode("ascii")
+f.write(content)
 
