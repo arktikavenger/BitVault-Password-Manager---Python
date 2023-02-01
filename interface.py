baf316a92946dd5daefe1cee9c6e92794a7addc1
Abdon Morales Jr <abdon.morales@moralesresearch.org>
2023-01-31 22:42:15 -0600

    Adding new tests (testing the font, etc.)

--- interface.py
+++ interface.py
@@ -3,7 +3,10 @@
 import PySimpleGUI as sg
 import tkinter as tk
 
-layout = [[sg.Text("Hello from TestGUI")], [sg.Text("(C) 1987 Sun Microsystems Inc")], [sg.Button("OK")]]
+font1 = ("Helvetica", 24)
+font2 = ("Bauhaus 93", 14)
+
+layout = [[sg.Text("Hello from TestGUI", size=(20,1), key='-text-', font=font1)], [sg.Text("(C) 1987 Sun Microsystems Inc", font=font2)], [sg.Button("OK")]]
 
 window = sg.Window("TEST", layout)
 
